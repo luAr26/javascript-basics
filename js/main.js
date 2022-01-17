ebd16d85351c6a883555efd0baad6208f9ef5d6e
luAr26 <raul_cosmin_26@yahoo.com>
2022-01-17 20:45:59 +0200

    Arrays, objects and performing operations

--- js/main.js
+++ js/main.js
@@ -3,3 +3,15 @@ let greeting = "Hi my name is Max";
 let myNumber = 32;
 console.log(greeting);
 console.log(myNumber);
+
+// Arrays
+let hobbies = ["Sports", "Cooking", "Reading"];
+console.log(hobbies[0]);
+
+// Objects
+let job = { title: "Web developer", place: "Munich", salary: 50000 };
+console.log(job.title);
+
+// Performing Operations
+let adultYears = myNumber - 18;
+console.log(adultYears);
