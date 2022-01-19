656b7bbbfe518c27e913b2347597df2abb9c2ded
luAr26 <raul_cosmin_26@yahoo.com>
2022-01-19 20:55:16 +0200

    DOM querying and drilling

--- js/dom.js
+++ js/dom.js
@@ -1,3 +1,6 @@
 console.dir(document);
 document.body.children[1].children[0].children[1].children[0].href =
   "https://www.google.com";
+
+const linkEl = document.querySelector("#external-link");
+linkEl.href = "https://yahoo.com";
